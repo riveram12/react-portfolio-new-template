c8cc9fa1efcfb32819c27253e72730c72165a749
Mariah Rivera <rivmariah06@gmail.com>
2021-04-10 18:42:08 -0400

    Updates

---
+++ precache-manifest.4705439fe768400f8cc4be34df9b4da1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d876683704733258a27",
+    "url": "/react-portfolio-new-template/static/css/main.5f68b40b.chunk.css"
+  },
+  {
+    "revision": "5d876683704733258a27",
+    "url": "/react-portfolio-new-template/static/js/main.5d876683.chunk.js"
+  },
+  {
+    "revision": "6a98067c2f9b7bced5f5",
+    "url": "/react-portfolio-new-template/static/js/runtime~main.6a98067c.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/react-portfolio-new-template/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "a694ad31b1190f3d68ce3bb1b921ce9f",
+    "url": "/react-portfolio-new-template/index.html"
+  }
+];
